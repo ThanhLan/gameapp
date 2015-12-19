1443b307ce714bee7298901c4463a4f4950cdcf1
ThanhLan <ThanhLan@users.noreply.github.com>
2015-12-19 11:27:41 +0100

    Create Obstacle.swift

---
+++ Obstacle.swift
@@ -0,0 +1,13 @@
+//
+//  Obstacle.swift
+//  AppGame
+//
+//  Created by Mac User on 8/14/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import Foundation
+
+class Obstacle : CCNode {
+
+}
